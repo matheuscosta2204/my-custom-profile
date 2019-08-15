19cc9780154a9006457bf5fbf80a2617dd6de271
matheuscosta2204 <matheuscosta2204@gmail.com>
2019-08-15 16:52:54 -0300

    Background Included

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Element } from 'react-scroll';
 
 import './home.scss';
-import HeartBeating from '../../media/svg/HeartBeating';
-
-// import style from '../../style/style.json';
+import HeartBeating from '../../media/svg/heartBeating';
 
 const home = () => {
     return (
-        <Element id="home" style={styles.container}>
+        <Element id="home" style={styles.container} className="home">
             <div className="entrance-container">
                 <div className="entrance-text">
-                    <h2>Hi there, I am&nbsp;</h2>
+                    <h2>Hi there, I am</h2>
                     <h1>Matheus Costa!</h1>
                 </div>
                 <div className="breef-description">
                     <p>A developer who loves development.</p>
-                    <p>Javascript</p><HeartBeating />
+                    <div className="js-container">
+                        <p>Javascript!</p>
+                        <HeartBeating />
+                    </div>
                 </div>
             </div>
         </Element>
@@ -27,7 +28,7 @@ export default home;
 
 const styles = {
     container: {
-        backgroundImage: 'linear-gradient(to right, #282c34, #2b303a, #2f3440, #323846, #363c4c)',
+        //backgroundImage: 'linear-gradient(to right, #282c34, #2b303a, #2f3440, #323846, #363c4c)',
         height: '100vh',
         color: 'white',
     },
